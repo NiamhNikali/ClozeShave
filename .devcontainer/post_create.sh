b265964ce132d287b023c291261840703e10a263
NiamhNikali <niamh.nikali@protonmail.com>
2025-08-23 01:48:26 +0000

    Add mise installation to devcontainer config

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Here's one possible way to install dev tools?
+curl https://mise.run | sh
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+eval "$(~/.local/bin/mise activate bash)"
+mise install
