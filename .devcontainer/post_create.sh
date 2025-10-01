ce7f05e444ebdea40fffbfb50c1dd4249698eaf6
Niamh Nikali <niamh.nikali@proton.me>
2025-10-01 13:29:26 -0600

    Begin moving to podman pods for dev server

--- .devcontainer/post_create.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Here's one possible way to install dev tools?
-curl https://mise.run | sh
-echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
-echo 'eval "$(mise activate bash)"' >> ~/.bashrc
-eval "$(~/.local/bin/mise activate bash)"
-mise install
